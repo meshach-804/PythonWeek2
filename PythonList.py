6978ab159ee580597cab0868e2cf070dd20077f2
Meshack Ombogo <ombogomeshack6@gmail.com>
2025-03-26 12:54:26 +0300

    Python list operations

---
+++ PythonList.py
@@ -0,0 +1,8 @@
+my_list = []
+my_list.extend([10, 20, 30])  # adding multiple elements
+my_list.insert(1, 15) #inserting 15 mat index 1
+my_list.extend([50, 60, 70])
+my_list.pop(-1)  #removing the last element
+my_list.sort()  #sorting in ascending order
+my_list.index(30)
+print(my_list.index(30))
